4cd6fd09d4f77d3832156d433e3e144ec1afed49
fortune onyeka <ihedfortune@gmail.com>
2024-12-17 12:07:49 +0100

    made the component super dynamic and super reusable

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -11,28 +11,50 @@ const starContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "1",
-  margin: "0",
-};
-const StarRating = ({ maxRating = 5 }) => {
-  const [rating, setRating] = useState(0);
+const StarRating = ({
+  maxRating = 5,
+  color = ["#8B0000", "#DC143C", "#89CFF0", "#0000FF", "#FFD700"],
+  size = 25,
+  className,
+  messages = [],
+  defaultRating = 0
+}) => {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
+  const textStyle = {
+    lineHeight: "1",
+    margin: "0",
+    color: color[tempRating ? tempRating - 1 : rating - 1] || color[0],
+    fontSize: `${size}px`,
+  };
+
+  // Helper function to get the star color
+  const getStarColor = (index) => {
+    return color[tempRating ? tempRating - 1 : rating - 1] || color[0];
+  };
+
   return (
-    <div style={containerStyle}>
+    <div style={containerStyle} className={className}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
             handleRating={() => setRating(i + 1)}
-            full={tempRating  ?  tempRating  >= i + 1 : rating >= i + 1}
+            full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
             onHoverIn={() => setTempRating(i + 1)}
-            onHoverOut={() =>  setTempRating(0)}
+            onHoverOut={() => setTempRating(0)}
+            color={getStarColor(i)} // Dynamic star color
+            size={size}
           />
         ))}
       </div>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      {/* Dynamically display message based on tempRating or rating */}
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1] || ""
+          : ""}
+      </p>
     </div>
   );
 };
