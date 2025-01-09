4be1ccc656162359b4a396b6efb75ff5ba9aae57
fortune onyeka <ihedfortune@gmail.com>
2025-01-09 11:48:17 +0100

    add focus to the input element on keydown Enter when the focus isnt already on the current input element

--- src/components/navigation/Search.jsx
+++ src/components/navigation/Search.jsx
@@ -1,23 +1,31 @@
 import { useEffect, useRef } from "react";
 
 const Search = ({ query, setQuery }) => {
-
   // create a ref element to focus on the input when the component mounts
-  const inputEl = useRef(null)
+  const inputEl = useRef(null);
 
   useEffect(() => {
-    inputEl.current.focus()
-  },[])
+    const callBack = (e) => {
+      if (document.activeElement === inputEl.current) return;
+
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callBack);
+    return document.addEventListener("keydown", callBack);
+  }, []);
   return (
-    <input 
+    <input
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
       ref={inputEl}
-      />
-    
+    />
   );
 };
 
