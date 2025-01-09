1ce8ac4e4410676cdccd68dfce876249054aad97
fortune onyeka <ihedfortune@gmail.com>
2025-01-09 11:33:52 +0100

    create a ref element to focus on the input when the component mounts

--- src/components/navigation/Search.jsx
+++ src/components/navigation/Search.jsx
@@ -1,12 +1,23 @@
+import { useEffect, useRef } from "react";
+
 const Search = ({ query, setQuery }) => {
+
+  // create a ref element to focus on the input when the component mounts
+  const inputEl = useRef(null)
+
+  useEffect(() => {
+    inputEl.current.focus()
+  },[])
   return (
-    <input
+    <input 
       className="search"
       type="text"
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
-    />
+      ref={inputEl}
+      />
+    
   );
 };
 
