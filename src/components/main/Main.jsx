24cf4182f7803492242accbec90315ab3f8700f9
fortune onyeka <ihedfortune@gmail.com>
2025-01-10 07:43:12 +0100

    created custom hook useLocalStorageState to replace the initial localStorage code

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -4,6 +4,7 @@ import WatchedSummary from "../movies/WatchedSummary";
 import WatchedList from "../movies/WatchedList";
 import MovieDetails from "../movies/MovieDetails";
 import { useMovieContext } from "../../MovieContext";
+import { useLocalStorageStage } from "../../useLocalStorageState";
 
 // Utility function to convert runtime string to minutes
 const parseRuntime = (runtime) => {
@@ -45,11 +46,10 @@ const calculateTotalRuntime = (movies) => {
 const Main = ({ children }) => {
   const { selectedId } = useMovieContext();
   // const [watched, setWatched] = useState([]);
-  const [watched, setWatched] = useState(() => {
-    const storedWatched = localStorage.getItem("watched");
-    return JSON.parse(storedWatched);
-  });
 
+  const [watched, setWatched] = useLocalStorageStage([],"watched")
+
+  
   // Calculate statistics
   const avgImdbRating = calculateAverage(watched, "imdbRating");
   const avgUserRating = calculateAverage(watched, "userRating");
@@ -70,10 +70,7 @@ const Main = ({ children }) => {
   };
 
   // Saved watched list in the localStorage
-  useEffect(() => {
-    localStorage.setItem("watched", JSON.stringify(watched));
-  }, [watched]);
-
+  
   return (
     <main className="main">
       {children}
